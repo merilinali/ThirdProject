8779418d4501c356b67feab1c6aa3f55eb5f182a
Merilin Ali <merilinayhan@gmail.com>
2020-11-22 21:25:49 +0300

    TextRenk

---
+++ WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="$safeprojectname$.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    
+    </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Click" />
+        <p>
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </p>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Purple" />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Blue" />
+        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Pink" />
+        <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="Green" />
+        <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="Red" />
+        <asp:Button ID="Button8" runat="server" OnClick="Button8_Click" Text="Yellow" />
+        <asp:Button ID="Button9" runat="server" OnClick="Button9_Click" Text="Black" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace $safeprojectname$
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = TextBox1.Text;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.ForeColor= System.Drawing.Color.Purple;
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Label1.ForeColor = System.Drawing.Color.Blue;
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            Label1.ForeColor = System.Drawing.Color.Pink;
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            Label1.ForeColor = System.Drawing.Color.Green;
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            Label1.ForeColor = System.Drawing.Color.Red;
+        }
+
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            Label1.ForeColor = System.Drawing.Color.Yellow;
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            Label1.ForeColor = System.Drawing.Color.Black;
+        }
+    }
+}
